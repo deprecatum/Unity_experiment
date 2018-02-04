bf064c0d2971a349d4a9dd02fcf1d06aa267ba56
deprecatum <33914332+deprecatum@users.noreply.github.com>
2018-02-04 16:33:43 +0000

    Add files via upload

---
+++ New Unity Project/Assets/character.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	struct armor{
+		int weight=0;
+		int defense=0;
+	}
+
+	struct arm{
+		int damage=1;
+		int penetration = 0;
+	}
+
+	static string name;
+	static int hp;
+
+	static armor armor_set[6];
+
+	static arm hands[2];
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
